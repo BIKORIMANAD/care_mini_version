8847d93b5f1726768e7fd33c7d2a07b3c2fade0c
Ruberandinda Patience <ruberandindap@gmail.com>
2021-05-08 20:55:43 +0200

    Check if the new code can work

--- rcp/document_cbhi.php
+++ rcp/document_cbhi.php
@@ -99,6 +99,7 @@ require_once "../lib2/cssmenu/rcp_header.html";
 	  
 	  </td>--><td>
 	  <input type=button class=flatbtn-blu style='padding:0 10px;' id=generate value='Generate' />
+	  <input type=button class=flatbtn-blu style='padding:0 10px;' id=generate_bill value='Generate Bill' />
 	  </td></tr>
 	  </table>
 	  <table class=list-1><tr><td>
@@ -158,6 +159,19 @@ $(document).ready(function(){
 		}
 		$(".patient_found").html("Please Wait...<br /><img src='../images/loading.gif' />");
 		$(".patient_found").load("document_list_cbhi.php?key=" + $("#insurance").val() + "&day=" + $("#day").val() + "&month=" + $("#month").val() + "&year=" + $("#year").val() + "&post=" + $("#post").val() + "&filter=" + $("#filter_").val());
+	});
+
+	$("#generate_bill").click(function(e){
+		$("#ds").html("");
+		$("#filter_").val("");
+		$("#ds").removeClass("error");
+		if(!$("#insurance").val()){
+			$("#ds").addClass("error");
+			$("#ds").html("Select Insurance");
+			return e.preventDefault();
+		}
+		$(".patient_found").html("Please Wait...<br /><img src='../images/loading.gif' />");
+		$(".patient_found").load("document_list_cbhi_bill.php?key=" + $("#insurance").val() + "&day=" + $("#day").val() + "&month=" + $("#month").val() + "&year=" + $("#year").val() + "&post=" + $("#post").val() + "&filter=" + $("#filter_").val());
 	});
 	
 	//if the search button is clicked search the patient_found
